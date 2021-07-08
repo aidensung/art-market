eb9949b62d83117fc59ab304f2033e9178bf2197
Haeun Grace Jeong <grace.he.jeong@gmail.com>
2021-07-08 18:01:50 -0400

    navbar
    main navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import "./App.css";
-import ROUTES, { RenderRoutes } from "./routes/root-route";
-import Footer from "./shared-components/footer";
-import Header from "./shared-components/header";
+import './App.css';
+import ROUTES, { RenderRoutes } from './routes/root-route';
+import Footer from './shared-components/footer';
+import Header from './shared-components/header';
+import Navbar from './shared-components/navbar';
 
 function App() {
-  return (
-    <>
-      <Header />
-      <RenderRoutes routes={ROUTES} />
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<Navbar />
+			<Header />
+			<RenderRoutes routes={ROUTES} />
+			<Footer />
+		</>
+	);
 }
 
 export default App;
---
+++ src/shared-components/navbar/navbar.component.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { FaBars, FaSearch } from 'react-icons/fa';
+
+import {
+	MobileIcon,
+	Nav,
+	NavbarContainer,
+	NavBtn,
+	NavBtnLink,
+	NavItem,
+	NavLinks,
+	NavLogo,
+	NavMenu,
+	Form,
+	Input,
+} from './navbarElement';
+
+const Navbar = (props) => {
+	return (
+		<>
+			<Nav>
+				<NavbarContainer>
+					<NavLogo to="/">Art Market</NavLogo>
+					<MobileIcon>
+						<FaBars />
+					</MobileIcon>
+					<Form>
+						<Input type="text" placeholder="Search" />
+						{/* <input type="submit" value="Submit" /> */}
+					</Form>
+					<NavMenu>
+						<NavItem>
+							<NavLinks to="#">Category</NavLinks>
+						</NavItem>
+						<NavItem>
+							<NavLinks to="#">Location</NavLinks>
+						</NavItem>
+						<NavBtn>
+							<NavBtnLink to="/auth/login">Sign In</NavBtnLink>
+						</NavBtn>
+					</NavMenu>
+				</NavbarContainer>
+			</Nav>
+		</>
+	);
+};
+
+export default Navbar;
