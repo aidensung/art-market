9d15a2b33eeb3187b11bb8a587515434208f70d6
Haeun Grace Jeong <grace.he.jeong@gmail.com>
2021-07-15 16:54:15 -0400

    Create Sidebar UI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import ROUTES, { RenderRoutes } from './routes/root-route';
+
 import Footer from './shared-components/footer';
 import Header from './shared-components/header';
 import Navbar from './shared-components/navbar';
+import Sidebar from './shared-components/sidebar';
 
 function App() {
 	return (
 		<>
 			<Navbar />
+			<Sidebar />
 			<Header />
 			<RenderRoutes routes={ROUTES} />
 			<Footer />
