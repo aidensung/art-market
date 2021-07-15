4aa4f5700abf7ecce9ff66ac2bbc1a582dece662
Cayden, Eunseok <59014867+EJ-C@users.noreply.github.com>
2021-07-15 15:52:41 -0400

    added initial Dropdown Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import './App.css';
-import ROUTES, { RenderRoutes } from './routes/root-route';
-import Footer from './shared-components/footer';
-import Header from './shared-components/header';
-import Navbar from './shared-components/navbar';
+import "./App.css";
+import ROUTES, { RenderRoutes } from "./routes/root-route";
+import Footer from "./shared-components/footer";
+import Header from "./shared-components/header";
+import Navbar from "./shared-components/navbar";
 
 function App() {
 	return (
@@ -10,6 +10,54 @@ function App() {
 			<Navbar />
 			<Header />
 			<RenderRoutes routes={ROUTES} />
+			<RenderRoutes routes={ROUTES} />
+			<RenderRoutes routes={ROUTES} />
+			<RenderRoutes routes={ROUTES} />
+			<RenderRoutes routes={ROUTES} />
+			<RenderRoutes routes={ROUTES} />
+			<RenderRoutes routes={ROUTES} />
+			<RenderRoutes routes={ROUTES} />
+			<RenderRoutes routes={ROUTES} />
+			<RenderRoutes routes={ROUTES} />
+			<RenderRoutes routes={ROUTES} />
+			<RenderRoutes routes={ROUTES} />
+			<RenderRoutes routes={ROUTES} />
+			<RenderRoutes routes={ROUTES} />
+			<RenderRoutes routes={ROUTES} />
+			<RenderRoutes routes={ROUTES} />
+			<RenderRoutes routes={ROUTES} />
+			<RenderRoutes routes={ROUTES} />
+			<RenderRoutes routes={ROUTES} />
+			<RenderRoutes routes={ROUTES} />
+			<RenderRoutes routes={ROUTES} />
+			<RenderRoutes routes={ROUTES} />
+			<RenderRoutes routes={ROUTES} />
+			<RenderRoutes routes={ROUTES} />
+			<RenderRoutes routes={ROUTES} />
+			<RenderRoutes routes={ROUTES} />
+			<RenderRoutes routes={ROUTES} />
+			<RenderRoutes routes={ROUTES} />
+			<RenderRoutes routes={ROUTES} />
+			<RenderRoutes routes={ROUTES} />
+			<RenderRoutes routes={ROUTES} />
+			<RenderRoutes routes={ROUTES} />
+			<RenderRoutes routes={ROUTES} />
+			<RenderRoutes routes={ROUTES} />
+			<RenderRoutes routes={ROUTES} />
+			<RenderRoutes routes={ROUTES} />
+			<RenderRoutes routes={ROUTES} />
+			<RenderRoutes routes={ROUTES} />
+			<RenderRoutes routes={ROUTES} />
+			<RenderRoutes routes={ROUTES} />
+			<RenderRoutes routes={ROUTES} />
+			<RenderRoutes routes={ROUTES} />
+			<RenderRoutes routes={ROUTES} />
+			<RenderRoutes routes={ROUTES} />
+			<RenderRoutes routes={ROUTES} />
+			<RenderRoutes routes={ROUTES} />
+			<RenderRoutes routes={ROUTES} />
+			<RenderRoutes routes={ROUTES} />
+
 			<Footer />
 		</>
 	);
--- src/shared-components/navbar/navbarElement.js
+++ src/shared-components/navbar/navbarElement.js
@@ -1,6 +1,6 @@
-import styled from 'styled-components';
-import { Link as LinkRouter } from 'react-router-dom';
-import { Link as LinkScroll } from 'react-scroll';
+import styled from "styled-components";
+import { Link as LinkRouter } from "react-router-dom";
+import { Link as LinkScroll } from "react-scroll";
 
 export const Nav = styled.nav`
 	background: #000;
