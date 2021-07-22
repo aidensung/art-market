84d6db3005347fc919109c64b933b27b35b11d12
Cayden, Eunseok <59014867+EJ-C@users.noreply.github.com>
2021-07-21 22:00:34 -0400

    fixed overlaping header and footer

--- src/shared-components/footer/footer.styles.js
+++ src/shared-components/footer/footer.styles.js
@@ -1,12 +1,12 @@
 import styled from "styled-components";
 
 export const Box = styled.div`
-	padding: 60px 60px;
 	background: black;
-	position: fixed;
-	bottom: 0;
+	position: relative;
+	height: auto;
 	width: 100%;
-
+	padding-top: 18px;
+	padding-bottom: 18px;
 	@media (max-width: 1000px) {
 		padding: 70px 30px;
 	}
@@ -32,7 +32,7 @@ export const Column = styled.div`
 export const Row = styled.div`
 	display: grid;
 	grid-template-columns: repeat(auto-fill, minmax(185px, 1fr));
-	grid-gap: 20px;
+	grid-gap: 15px;
 
 	@media (max-width: 1000px) {
 		grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
@@ -42,7 +42,7 @@ export const Row = styled.div`
 export const FooterLink = styled.a`
 	color: #fff;
 	margin-bottom: 20px;
-	font-size: 18px;
+	font-size: 15px;
 	text-decoration: none;
 
 	&:hover {
@@ -52,7 +52,7 @@ export const FooterLink = styled.a`
 `;
 
 export const Heading = styled.p`
-	font-size: 24px;
+	font-size: 20px;
 	color: #fff;
 	margin-bottom: 40px;
 	font-weight: bold;
